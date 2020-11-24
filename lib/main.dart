61a99af1771adf9fa46ac4b6304cb07f2c83dc0c
riclzpt <ricardolpinto@gmail.com>
2020-11-24 15:55:31 -0300

    Primeiro Commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    title: "Contador de Pessoas",
+    home: Container(
+      color: Colors.amberAccent,
+    )
+  ));
+}
